363ac43b228fa069062b8cab66dab070e8114202
Nicole Barry <nmbarry15@gmail.com>
2018-10-22 18:43:11 -0400

    change connection

--- server.js
+++ server.js
@@ -18,13 +18,20 @@ app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
+
 
 connection.connect(function (err) {
     if (err) {
@@ -51,7 +58,7 @@ app.get("/", function (req, res) {
             }
         }
 
-        res.render("index", { burgers: burgers, dburgers:dburgers });
+        res.render("index", { burgers: burgers, dburgers: dburgers });
     });
 });
 
